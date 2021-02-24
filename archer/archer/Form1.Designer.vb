4bf4b2569f82ef84aae9dbbeb68c3eb9cee5131b
plusminus34 <48390907+plusminus34@users.noreply.github.com>
2021-02-24 09:59:27 +0100

    Add archer

---
+++ archer/archer/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Archery
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(181, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(300, 310)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(307, 355)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(100, 299)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Shoot"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(100, 273)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {100, 0, 0, -2147483648})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(75, 20)
+        Me.NumericUpDown1.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(487, 299)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Shoot"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(487, 273)
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {100, 0, 0, -2147483648})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(75, 20)
+        Me.NumericUpDown2.TabIndex = 5
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(97, 257)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Angle"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(484, 257)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Angle"
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(100, 234)
+        Me.NumericUpDown3.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(75, 20)
+        Me.NumericUpDown3.TabIndex = 8
+        Me.NumericUpDown3.Value = New Decimal(New Integer() {100, 0, 0, 0})
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(487, 234)
+        Me.NumericUpDown4.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(75, 20)
+        Me.NumericUpDown4.TabIndex = 9
+        Me.NumericUpDown4.Value = New Decimal(New Integer() {100, 0, 0, 0})
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(97, 218)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Power"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(487, 218)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Power"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Arryn", "Baratheon", "Greyjoy", "Lannister", "Martell", "Stark", "Targaryen", "Tully", "Tyrell"})
+        Me.ComboBox1.Location = New System.Drawing.Point(90, 117)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(85, 21)
+        Me.ComboBox1.TabIndex = 14
+        Me.ComboBox1.Text = "Stark"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Arryn", "Baratheon", "Greyjoy", "Lannister", "Martell", "Stark", "Targaryen", "Tully", "Tyrell"})
+        Me.ComboBox2.Location = New System.Drawing.Point(487, 117)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(85, 21)
+        Me.ComboBox2.TabIndex = 15
+        Me.ComboBox2.Text = "Lannister"
+        '
+        'Archery
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(655, 486)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.NumericUpDown4)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Archery"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown4 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ archer/archer/Form1.vb
@@ -0,0 +1,248 @@
+﻿Public Class Archery
+    Dim bild As Graphics
+    Dim a As Single
+    Dim x1 As Single
+    Dim y1 As Single
+    Dim x2 As Single
+    Dim y2 As Single
+    Dim xp(2) As Single
+    Dim yp(2) As Single
+    Dim vx As Single
+    Dim vy As Single
+    Dim h1 As Single
+    Dim h2 As Single
+    Dim wind As Single
+    Dim rot As Integer
+    Dim blau As Integer
+    Dim gruen As Integer
+
+    Dim flying As Boolean
+
+    Sub drawguy(house As Integer, x As Single, y As Single)
+        Select Case house
+            Case 1
+                bild.FillEllipse(New SolidBrush(Color.White), x, y, 20, 20)
+                bild.FillEllipse(New SolidBrush(Color.LightGray), x + 5, y + 8, 10, 4)
+            Case 2
+                bild.FillEllipse(New SolidBrush(Color.Crimson), x, y, 20, 20)
+                bild.FillEllipse(New SolidBrush(Color.Gold), x + 5, y + 7, 10, 6)
+            Case 3
+                bild.FillEllipse(New SolidBrush(Color.Black), x, y, 20, 20)
+                bild.FillEllipse(New SolidBrush(Color.Red), x + 5, y + 7, 10, 6)
+            Case 4
+                bild.FillEllipse(New SolidBrush(Color.RoyalBlue), x, y, 20, 20)
+                bild.FillEllipse(New SolidBrush(Color.White), x + 6, y + 6, 8, 8)
+                bild.FillEllipse(New SolidBrush(Color.RoyalBlue), x + 8, y + 8, 4, 4)
+            Case 5
+                bild.FillEllipse(New SolidBrush(Color.Gold), x, y, 20, 20)
+                bild.FillEllipse(New SolidBrush(Color.Black), x + 6, y + 5, 6, 10)
+            Case 6
+                bild.FillEllipse(New SolidBrush(Color.Black), x, y, 20, 20)
+                bild.FillEllipse(New SolidBrush(Color.Gold), x + 6, y + 2, 8, 14)
+            Case 7
+                bild.FillEllipse(New SolidBrush(Color.Orange), x, y, 20, 20)
+                bild.FillEllipse(New SolidBrush(Color.Red), x + 6, y + 6, 8, 8)
+                bild.DrawLine(New Pen(Color.Gold), x + 5, y + 5, x + 15, y + 15)
+            Case 8
+                bild.FillEllipse(New SolidBrush(Color.Red), x, y, 20, 20)
+                bild.DrawLine(New Pen(Color.Blue, 4), x + 7, y + 2, x + 7, y + 19)
+                bild.DrawLine(New Pen(Color.Blue, 4), x + 15, y + 2, x + 15, y + 19)
+                bild.DrawLine(New Pen(Color.White, 3), x + 13, y + 15, x + 7, y + 5)
+            Case 9
+                bild.FillEllipse(New SolidBrush(Color.Green), x, y, 20, 20)
+                bild.FillEllipse(New SolidBrush(Color.Gold), x + 7, y + 7, 6, 6)
+        End Select
+    End Sub
+
+    Sub drawflag()
+        bild.FillRectangle(New SolidBrush(Color.FromArgb(200, 230, 255)), 136, 260, 27, 40)
+        bild.DrawLine(New Pen(Color.Brown, 3), 150, 300, 150, 260)
+        If wind > 0 Then
+            bild.FillRectangle(New SolidBrush(Color.FromArgb(rot, blau, gruen)), 150, 260, (wind * 50), 10)
+        Else
+            bild.FillRectangle(New SolidBrush(Color.FromArgb(rot, blau, gruen)), 150 + wind * 80, 260, (wind * -80), 10)
+
+        End If
+    End Sub
+
+    Sub setwind()
+        wind = (Rnd() * 100 - 50) / 300
+    End Sub
+
+    Sub win(house As Integer)
+        Select Case house
+            Case 1
+                bild.DrawString("Winter is coming.", New Font("Arial", 20), New SolidBrush(Color.White), 10, 20)
+            Case 2
+                bild.DrawString("Hear me roar!", New Font("Arial", 20), New SolidBrush(Color.Crimson), 10, 20)
+            Case 3
+                bild.DrawString("Fire and Blood.", New Font("Arial", 20), New SolidBrush(Color.Black), 10, 20)
+            Case 4
+                bild.DrawString("As high as honour.", New Font("Arial", 20), New SolidBrush(Color.White), 10, 20)
+            Case 5
+                bild.DrawString("Ours is the fury!", New Font("Arial", 20), New SolidBrush(Color.Black), 10, 20)
+            Case 6
+                bild.DrawString("We do not sow.", New Font("Arial", 20), New SolidBrush(Color.Black), 10, 20)
+            Case 7
+                bild.DrawString("Unbowed, unbent, unbroken.", New Font("Arial", 16), New SolidBrush(Color.Red), 10, 20)
+            Case 8
+                bild.DrawString("Family, honour, duty.", New Font("Arial", 20), New SolidBrush(Color.Red), 10, 20)
+            Case 9
+                bild.DrawString("Growing strong.", New Font("Arial", 20), New SolidBrush(Color.Green), 10, 20)
+        End Select
+    End Sub
+
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Randomize()
+        rot = (Rnd() * 255)
+        blau = (Rnd() * 255)
+        gruen = (Rnd() * 255)
+        ComboBox1.Enabled = False
+        ComboBox2.Enabled = False
+        Call setwind()
+        a = Math.Ceiling(Rnd() * 200)
+        bild = PictureBox1.CreateGraphics
+        x1 = 20
+        y1 = 280 - a
+        a = Math.Ceiling(Rnd() * 200)
+        x2 = 260
+        y2 = 280 - a
+        bild.FillRectangle(New SolidBrush(Color.FromArgb(200, 230, 255)), 0, 0, 300, 300)
+        bild.FillRectangle(New SolidBrush(Color.FromArgb(20, 255, 0)), 0, 300, 300, 10)
+        a = Math.Ceiling(Rnd() * 150) + 50
+        bild.FillRectangle(New SolidBrush(Color.FromArgb(a, a, a)), x1, y1 + 20, 20, 300 - (y1 + 20))
+        a = Math.Ceiling(Rnd() * 150) + 50
+        bild.FillRectangle(New SolidBrush(Color.FromArgb(a, a, a)), x2, y2 + 20, 20, 300 - (y2 + 20))
+        Call drawguy(h1, x1, y1)
+        Call drawguy(h2, x2, y2)
+        Call drawflag()
+        Button2.Enabled = True
+        Button3.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        xp(0) = x1 + 21
+        yp(0) = y1 + 10
+        xp(1) = x1 + 21
+        yp(1) = y1 + 10
+        xp(2) = x1 + 21
+        yp(2) = y1 + 10
+        flying = True
+        Timer1.Enabled = True
+        vx = NumericUpDown3.Value * Math.Cos(NumericUpDown1.Value / 180 * Math.PI) / 10
+        vy = NumericUpDown3.Value * Math.Sin(-NumericUpDown1.Value / 180 * Math.PI) / 10
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        xp(0) = xp(1)
+        yp(0) = yp(1)
+        xp(1) = xp(2)
+        yp(1) = yp(2)
+        xp(2) += vx
+        yp(2) += vy
+        vy += 0.2
+        vx += wind
+        bild.DrawLine(New Pen(Color.Brown), xp(2), yp(2), xp(1), yp(1))
+        bild.DrawLine(New Pen(Color.FromArgb(200, 230, 255)), xp(1), yp(1), xp(0), yp(0))
+        Call drawflag()
+        If xp(2) > 310 Or xp(2) <= -10 Or yp(2) >= 300 Then
+            flying = False
+            Timer1.Enabled = False
+            Call setwind()
+            Call drawflag()
+        End If
+        If xp(2) > x1 And xp(2) <= x1 + 20 And yp(2) >= y1 + 20 Then
+            flying = False
+            Timer1.Enabled = False
+            Call setwind()
+            Call drawflag()
+        End If
+        If xp(2) > x2 And xp(2) <= x2 + 20 And yp(2) >= y2 + 20 Then
+            flying = False
+            Timer1.Enabled = False
+            Call setwind()
+            Call drawflag()
+        End If
+        If xp(2) >= x2 And xp(2) < x2 + 20 And yp(2) >= y2 And yp(2) < y2 + 20 Then
+            flying = False
+            Timer1.Enabled = False
+            Button3.Enabled = False
+            Call win(h1)
+            ComboBox1.Enabled = True
+            ComboBox2.Enabled = True
+        End If
+        If xp(2) >= x1 And xp(2) < x1 + 20 And yp(2) >= y1 And yp(2) < y1 + 20 Then
+            flying = False
+            Timer1.Enabled = False
+            Button2.Enabled = False
+            Call win(h2)
+            ComboBox1.Enabled = True
+            ComboBox2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        xp(0) = x2 - 1
+        yp(0) = y2 + 10
+        xp(1) = x2 - 1
+        yp(1) = y2 + 10
+        xp(2) = x2 - 1
+        yp(2) = y2 + 10
+        flying = True
+        Timer1.Enabled = True
+        vx = -NumericUpDown4.Value * Math.Cos(NumericUpDown2.Value / 180 * Math.PI) / 10
+        vy = NumericUpDown4.Value * Math.Sin(-NumericUpDown2.Value / 180 * Math.PI) / 10
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        h1 = 1
+        h2 = 2
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Select Case ComboBox1.Text
+            Case "Stark"
+                h1 = 1
+            Case "Lannister"
+                h1 = 2
+            Case "Targaryen"
+                h1 = 3
+            Case "Arryn"
+                h1 = 4
+            Case "Baratheon"
+                h1 = 5
+            Case "Greyjoy"
+                h1 = 6
+            Case "Martell"
+                h1 = 7
+            Case "Tully"
+                h1 = 8
+            Case "Tyrell"
+                h1 = 9
+        End Select
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ComboBox2.SelectedIndexChanged
+        Select Case ComboBox2.Text
+            Case "Stark"
+                h2 = 1
+            Case "Lannister"
+                h2 = 2
+            Case "Targaryen"
+                h2 = 3
+            Case "Arryn"
+                h2 = 4
+            Case "Baratheon"
+                h2 = 5
+            Case "Greyjoy"
+                h2 = 6
+            Case "Martell"
+                h2 = 7
+            Case "Tully"
+                h2 = 8
+            Case "Tyrell"
+                h2 = 9
+        End Select
+    End Sub
+End Class
